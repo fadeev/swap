c233880e6489f664d27f0bbe7d01f18e3951f842
Denis Fadeev <denis@fadeev.org>
2024-07-10 17:14:44 +0300

    init

--- src/components/Swap/index.tsx
+++ src/components/Swap/index.tsx
@@ -1,3 +1,10 @@
+// Swap component being imported
+
+"use client";
+
+import { useAccount } from "wagmi";
+
 export const Swap = () => {
+  const { address } = useAccount();
   return <div>Swap</div>;
 };
